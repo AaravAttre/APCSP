68550e9cd40b8bb9601a3652a3dffc940d4c82c8
Aarav Attre <aarav.attre@gmail.com>
2024-03-16 18:05:07 -0700

    Adding APCSA projects

---
+++ APCSA/Chapter 1 - Intro to Java/1.4 - Java Coding/AppMain.java
@@ -0,0 +1,8 @@
+public class AppMain {
+    public static void main(String[] args) {
+        AsciiArt art = new AsciiArt();
+        art.printFish();
+        art.printCat();
+        // TODO
+    }
+}
